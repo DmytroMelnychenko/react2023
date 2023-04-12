f3b81b982755b0c54f24a5f1497ae078286bce90
DmytroMelnychenko <meldimld@gmail.com>
2023-04-12 15:38:47 +0300

    master

--- src/App.js
+++ src/App.js
@@ -12,3 +12,13 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// 1. Описати всю сім'ю сімпсонів (5 персонажів)
+//
+// 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
+// https://rickandmortyapi.com/
+//     https://rickandmortyapi.com/api/character
+//         Створити 6 персонажів
